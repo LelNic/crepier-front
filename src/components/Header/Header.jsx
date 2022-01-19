834252a508f3f190ccf132e4c3db457f40e2f488
OEM Configuration (temporary user) <Lel/Nic>
2022-01-19 21:39:36 +0100

    ajout logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Header.css";
+import disco from "../../assets/images/discoCrepeLogo.png";
 
 const Header = () => {
   return (
     <header>
+      <img src={disco} alt="Logo de disco crêpe" />
       <div>Disco Crêpe</div>
     </header>
   );
