c6c9450efbd6d8b6ab29a381b0a05340ced27c3e
OEM Configuration (temporary user) <Lel/Nic>
2022-01-20 00:54:28 +0100

    ajout item

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,12 +1,23 @@
+// import { Link } from "react-router-dom";
 import React from "react";
 import "./Item.css";
+import Button from "./../Button/Button";
 
-const Item = ({ name }) => {
-    return (
-        <li className="item">
-            <h2>{name}</h2>
-        </li>
-    );
+const Item = ({ name, shortDescribe, price, image }) => {
+  return (
+    <div>
+      <li className="item">
+        <h2>{name}</h2>
+        <h3>{image}</h3>
+        <h3>{shortDescribe}</h3>
+        <h3>{price}</h3>
+      </li>
+      {/* <Link to={`/services/${data.id}`}>
+          <span className="link-service-card">Afficher plus...</span>
+        </Link> */}
+      <Button text="Commander la crepe" />
+    </div>
+  );
 };
 
 export default Item;
