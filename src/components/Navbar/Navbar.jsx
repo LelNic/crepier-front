4e4dc7dee3d7ce22cab671c1a0f76a547d5baf6c
OEM Configuration (temporary user) <Lel/Nic>
2022-01-20 14:33:39 +0100

    panier

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,66 +1,67 @@
 import { Link } from "react-router-dom";
-import { BsPersonCircle } from "react-icons/bs";
+import { BsPersonCircle, BsCardChecklist } from "react-icons/bs";
 import { ImCross, ImUserPlus } from "react-icons/im";
 import FaOpencart from "../../assets/images/carticon.png";
-import logo from "../../assets/images/discoBoule.png";
+import logo from "../../assets/images/discoboule.png";
 import "./Navbar.css";
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext.js";
 import { UserContext } from "../../contexts/UserContext.js";
 
 const Navbar = () => {
-    const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
-    const { user, setUser } = useContext(UserContext);
+  const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
+  const { user, setUser } = useContext(UserContext);
 
-    const logout = () => {
-        localStorage.clear();
-        setIsAuthenticated(false);
-        setUser({});
-        window.location.href = "/";
-    };
+  const logout = () => {
+    localStorage.clear();
+    setIsAuthenticated(false);
+    setUser({});
+    window.location.href = "/";
+  };
 
-    return (
-        <nav className="Navbar">
-            <ul className="menu">
-                {isAuthenticated ? (
-                    <>
-                        <Link to="/user/profile">
-                            <li className="menu-container">
-                                <BsPersonCircle className="nav-icon" />
-                                <span>{user.firstname}</span>
-                            </li>
-                        </Link>
-                        <ImCross className="nav-icon red" onClick={logout} />
-                    </>
-                ) : (
-                    <>
-                        <Link to="/login">
-                            <li className="menu-container">
-                                <BsPersonCircle className="nav-icon" />
-                                <span>Login</span>
-                            </li>
-                        </Link>
-                        <Link to="/register">
-                            <li className="menu-container">
-                                <ImUserPlus className="nav-icon" />
-                                <span>Inscription</span>
-                            </li>
-                        </Link>
-                    </>
-                )}
-                <Link to="/" className={`mobile`}>
-                    <li>
-                        <img src={logo} alt="Logo du site Disco Crêpe" className="logoHome" />
-                    </li>
-                </Link>
-                <Link to="/" className={`mobile`}>
-                    <li>
-                        <img src={FaOpencart} alt="picturecartpng" className="nav-icon-cart" />
-                    </li>
-                </Link>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="Navbar">
+      <ul className="menu">
+        {isAuthenticated ? (
+          <>
+            <Link to="/user/profile">
+              <li className="menu-container">
+                <BsPersonCircle className="nav-icon" />
+                <span>{user.firstname}</span>
+              </li>
+            </Link>
+            <ImCross className="nav-icon red" onClick={logout} />
+          </>
+        ) : (
+          <>
+            <Link to="/login">
+              <li className="menu-container">
+                <BsPersonCircle className="nav-icon" />
+                <span>Login</span>
+              </li>
+            </Link>
+            <Link to="/register">
+              <li className="menu-container">
+                <ImUserPlus className="nav-icon" />
+                <span>Inscription</span>
+              </li>
+            </Link>
+          </>
+        )}
+        <Link to="/" className={`mobile`}>
+          <li>
+            <img src={logo} alt="Logo du site Disco Crêpe" className="logoHome" />
+          </li>
+        </Link>
+        <BsCardChecklist className="nav-icon" />
+        <Link to="/" className={`mobile`}>
+          <li>
+            <img src={FaOpencart} alt="picturecartpng" className="nav-icon-cart" />
+          </li>
+        </Link>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/services/services.js
+++ src/services/services.js
@@ -2,4 +2,19 @@ const strLimit = (string) => {
   return `${string.substring(0, 150)} [...] `;
 };
 
-export default strLimit;
+const addToCart = (id, price) => {
+  let products = [];
+  if (localStorage.getItem("products")) {
+    products = JSON.parse(localStorage.getItem("products"));
+  }
+  products.push({ productsId: id, price: price });
+  localStorage.setItem("products", JSON.stringify(products));
+};
+
+const removeFromCart = (id) => {
+  let storageProducts = JSON.parse(localStorage.getItem("products"));
+  let products = storageProducts.filter((product) => product.id !== id);
+  localStorage.setItem("products", JSON.stringify(products));
+};
+
+export default { strLimit, addToCart, removeFromCart };
